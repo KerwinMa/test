54f2166ed6ae334e14c2512cfd6a929f6cb29747
kamuszhou <zausiu@gmail.com>
2014-01-13 13:42:10 +0800

    exodus

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <memory.h>
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << strerror(10) << endl;
+
+	return 0;
+}
